0fe660359ee9d01c2b6aa57b4cb3932144e738f8
Shane Gough <shane.gough@kaelus.com>
2013-08-20 17:12:36 +1000

    Started fleshing out the source tree to build up the framework.

---
+++ include/clixx.h
@@ -0,0 +1,8 @@
+/*--------------------------------------------------------------------------*
+* ClixxLib - Copyright (c) 2013, Shane Gough (shane@thegaragelab.com)
+*            For licensing information see COPYING in the project root.
+*---------------------------------------------------------------------------*
+* 20-Aug-2013 ShaneG
+*
+* The main header file for the Clixx library.
+*--------------------------------------------------------------------------*/
---
+++ src/docks.cpp
@@ -0,0 +1,9 @@
+/*--------------------------------------------------------------------------*
+* ClixxLib - Copyright (c) 2013, Shane Gough (shane@thegaragelab.com)
+*            For licensing information see COPYING in the project root.
+*---------------------------------------------------------------------------*
+* 20-Aug-2013 ShaneG
+*
+* This implements the common methods for the Dock classes and their
+* derivitives.
+*--------------------------------------------------------------------------*/
