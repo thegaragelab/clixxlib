40e355f7d75aa3ee1e18cf21fb01193d9fb5333d
Shane Gough <shane.gough@kaelus.com>
2013-09-13 16:23:33 +1000

    Updated protection on main board file.

--- include/clixx/boards.h
+++ include/clixx/boards.h
@@ -7,3 +7,13 @@
 * This file is used to bring in the board specific definitions from the
 * appropriate include files.
 *--------------------------------------------------------------------------*/
+#ifndef __CLIXX_BOARDS_H
+#define __CLIXX_BOARDS_H
+
+// Do some sanity checking
+#ifndef __CLIXX_H
+#  error "Do not include this file directly. Include <clixx.h> instead."
+#endif
+
+#endif // __CLIXX_BOARDS_H
+
